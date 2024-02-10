d8f348d8b0575e9879e9fb9b76be27c55989d4c7
Muhammad Aditya <80536575+aditpee@users.noreply.github.com>
2024-02-10 17:26:20 +0700

    Update Home.js, make Marquee photos slower

--- src/Home.js
+++ src/Home.js
@@ -51,7 +51,7 @@ const Home = () => {
           centeredSlides={true}
           allowTouchMove={false}
           loop={true}
-          speed={2000}
+          speed={3000}
           autoplay={{
             delay: 1,
             // pauseOnMouseEnter: true,
